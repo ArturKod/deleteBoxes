fc6be8346c746715dae7b93a9cb4b7ce3c1ba357
ArturKod <127652742+ArturKod@users.noreply.github.com>
2023-04-06 23:23:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let boxes = document.querySelectorAll('.box');
+function deleteBox() {
+    event.target.remove();
+}
+for (const box of boxes) {
+    box.style.cursor = 'pointer';
+    let num = +(box.innerText);
+    console.log(num);
+    box.addEventListener('click', function () {
+        box.innerText = --num;
+        if(num == 0) {
+            box.addEventListener('click', deleteBox);
+        }  
+    });
+ 
+}
